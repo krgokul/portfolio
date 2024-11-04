67362f8c22aee2c79f27bd343a8d48b6401e6192
Gokul K R <gokul.kr@bigthinkcode.com>
2024-11-05 00:00:19 +0530

    initial design

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function () {
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.toggle("hidden");
+});
